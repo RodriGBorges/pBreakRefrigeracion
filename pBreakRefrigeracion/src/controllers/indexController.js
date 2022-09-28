7d6d70c88a4185d00acd22fb975645e8e2a9f8e6
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2022-09-28 14:45:01 -0300

    Estructura inicial del home

--- pBreakRefrigeracion/src/controllers/indexController.js
+++ pBreakRefrigeracion/src/controllers/indexController.js
@@ -1,8 +1,8 @@
-//const clientes = require('../data/clientes.json');
+const clientes = require('../data/clientes.json');
 //const trabajos = require('../data/trabajos.json');
 
 module.exports = {
     home: (req, res) => {
-        res.render('index', { title: 'Presupuestos BreakR' });
+        res.render('index', { clientes });
     }
 }
