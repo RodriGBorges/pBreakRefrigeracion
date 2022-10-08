5801b7ed3aa00ba64142268127bcfffcb37a0a98
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2022-10-08 17:03:27 -0300

    Ruta dinamica para editar presupuesto

--- pBreakRefrigeracion/src/routes/adminRouter.js
+++ pBreakRefrigeracion/src/routes/adminRouter.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-const { vistaCrear } = require('../controllers/adminController');
+const { vistaCrear, vistaEditar } = require('../controllers/adminController');
 
 /* ruta crear presupuesto */
 router.get('/crear', vistaCrear);
+router.get('/editar/:id', vistaEditar);
 
 module.exports = router;
