a52607631c47c7c46c5617452ae7bda4bd1a1f45
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2022-10-08 17:02:55 -0300

    Funcion para vista editar presupuesto

--- pBreakRefrigeracion/src/controllers/adminController.js
+++ pBreakRefrigeracion/src/controllers/adminController.js
@@ -1,5 +1,21 @@
+const clientes = require('../data/clientes.json');
+const trabajos = require('../data/trabajos.json');
+
 module.exports = {
     vistaCrear: (req, res) => {
         res.render('admin/crear');
+    },
+    vistaEditar: (req, res) => {
+        const {id} = req.params;
+        const presupuesto = clientes.find(cliente => cliente.id === parseInt(id));
+
+        let trabajosCliente = [];
+        for (let i = 0; i < trabajos.length; i++) {
+            if(trabajos[i].id == id) {
+                trabajosCliente.push(trabajos[i])
+            }
+        }
+
+        res.render('admin/editar', {presupuesto, trabajosCliente})
     }
 }
