eb6b313caca0e653d40460fa3f65021ff6c01962
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2022-10-15 23:26:44 -0300

    Funcion de botones de vista crear

--- pBreakRefrigeracion/public/javascripts/general.js
+++ pBreakRefrigeracion/public/javascripts/general.js
@@ -5,9 +5,20 @@ window.addEventListener('load', () => {
         return document.querySelector(tag)
     }
 
+    // Query selector All //
+    const qsAll = (tag) => {
+        return document.querySelectorAll(tag)
+    }
+
     const menuBurger = qs('#bttnBurger');
     const nav = qs('#navMobile');
     const bttnClose = qs('#bttnBurgerClose');
+    const bttnMasTrabajos = qs('#masTrabajos');
+    const bttnDescripcion = qs('#bttnDescripcion');
+    const mostrarTrabajos = qsAll('.mostrarTrabajos');
+    const titleDescripcion = qs('.descripcion');
+    const divDescripcion = qs('.divDescripcion');
+
 
     menuBurger.addEventListener('click', () => {
         nav.style.display = 'flex';
@@ -19,5 +30,21 @@ window.addEventListener('load', () => {
         nav.style.display = 'none';
         bttnClose.style.display = 'none';
         menuBurger.style.display = 'block';
+    })
+
+    //Vista crear
+    bttnMasTrabajos.addEventListener('click', (e) => {
+        e.preventDefault();
+        for (let i = 0; i < mostrarTrabajos.length; i++) {
+            mostrarTrabajos[i].style.display = 'flex';
+        }
+        bttnMasTrabajos.style.display = 'none';
+    })
+
+    bttnDescripcion.addEventListener('click', (e) => {
+        e.preventDefault();
+        titleDescripcion.style.display = 'block';
+        divDescripcion.style.display = 'block';
+        bttnDescripcion.style.display = 'none';
     })
 })
