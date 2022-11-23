a1a0f3121f57b800bd35f47ec47500907b784d3a
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2022-11-23 15:39:41 -0300

    Funcion editar presupuesto

--- pBreakRefrigeracion/src/controllers/adminController.js
+++ pBreakRefrigeracion/src/controllers/adminController.js
@@ -66,5 +66,57 @@ module.exports = {
         fs.writeFileSync(presupuestosDestino, JSON.stringify(presupuestos, null, 2));
 
         res.redirect(`/presupuesto/${nuevoPresupuesto.id}`);
+    },
+    editarPresupuesto: (req, res) => {
+
+        let presupuestoEditado = presupuestos.find(presupuesto => presupuesto.id === parseInt(req.params.id));
+        let { clave, nombre, direccion, telefono, garantia, actualizarFecha, descripcion, trabajo0, precio0, trabajo1, precio1, trabajo2, precio2, trabajo3, precio3, trabajo4, precio4, trabajo5, precio5, trabajo6, precio6, trabajo7, precio7, trabajo8, precio8, trabajo9, precio9, trabajo10, precio10, trabajo11, precio11 } = req.body;
+
+        let totalPrecio = 0;
+        let precios = [precio0, precio1, precio2, precio3, precio4, precio5, precio6, precio7, precio8, precio9, precio10, precio11];
+        precios.forEach(precio => {
+            if (precio != '') {
+                totalPrecio += parseInt(precio)
+            }
+        })
+
+        actualizarFecha == "on" ? presupuestoEditado.fechaPresupuesto = moment().format('DD/MM/YYYY, H:mm a') : null;
+
+        presupuestoEditado.palabraClave = clave == "" ? 0 : clave;
+        presupuestoEditado.nombreCliente = nombre;
+        presupuestoEditado.direccionCliente = direccion;
+        presupuestoEditado.telefonoCliente = telefono;
+        presupuestoEditado.garantia = garantia;
+        presupuestoEditado.descripcion = descripcion == "" ? 0 : descripcion;
+        presupuestoEditado.precioTrabajoTotal = totalPrecio;
+        presupuestoEditado.trabajo0 = trabajo0;
+        presupuestoEditado.precio0 = precio0;
+        presupuestoEditado.trabajo1 = trabajo1;
+        presupuestoEditado.precio1 = precio1;
+        presupuestoEditado.trabajo2 = trabajo2;
+        presupuestoEditado.precio2 = precio2;
+        presupuestoEditado.trabajo3 = trabajo3;
+        presupuestoEditado.precio3 = precio3;
+        presupuestoEditado.trabajo4 = trabajo4;
+        presupuestoEditado.precio4 = precio4;
+        presupuestoEditado.trabajo5 = trabajo5;
+        presupuestoEditado.precio5 = precio5;
+        presupuestoEditado.trabajo6 = trabajo6;
+        presupuestoEditado.precio6 = precio6;
+        presupuestoEditado.trabajo7 = trabajo7;
+        presupuestoEditado.precio7 = precio7;
+        presupuestoEditado.trabajo8 = trabajo8;
+        presupuestoEditado.precio8 = precio8;
+        presupuestoEditado.trabajo9 = trabajo9;
+        presupuestoEditado.precio9 = precio9;
+        presupuestoEditado.trabajo10 = trabajo10;
+        presupuestoEditado.precio10 = precio10;
+        presupuestoEditado.trabajo11 = trabajo11;
+        presupuestoEditado.precio11 = precio11;
+
+        fs.writeFileSync(presupuestosDestino, JSON.stringify(presupuestos, null, 2));
+
+        res.redirect(`/presupuesto/${req.params.id}`);
+
     }
 }
--- pBreakRefrigeracion/src/routes/adminRouter.js
+++ pBreakRefrigeracion/src/routes/adminRouter.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
-const { vistaCrear, vistaEditar, crearPresupuesto } = require('../controllers/adminController');
+const { vistaCrear, vistaEditar, crearPresupuesto, editarPresupuesto } = require('../controllers/adminController');
 
 /* ruta crear presupuesto */
 router.get('/crear', vistaCrear);
 router.get('/editar/:id', vistaEditar);
 
 router.post('/crear', crearPresupuesto);
+router.put('/editar/:id', editarPresupuesto);
 
 module.exports = router;
