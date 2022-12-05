836a04312446e94476e67b2a9f6909b66c025421
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2022-12-05 05:46:42 -0300

    Funcion pasar a mil para el home

--- pBreakRefrigeracion/src/controllers/indexController.js
+++ pBreakRefrigeracion/src/controllers/indexController.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 const path = require('path');
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const presupuestosDestino = path.join(__dirname, '../data/presupuestos.json');
 let presupuestos = JSON.parse(fs.readFileSync(presupuestosDestino, "utf-8"));
 
 module.exports = {
     home: (req, res) => {
-        res.render('index', { presupuestos });
+        res.render('index', { presupuestos, toThousand });
     }
 }
