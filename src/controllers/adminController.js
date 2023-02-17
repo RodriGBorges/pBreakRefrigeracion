39069ff186880b2a867faee68f09eb0ecf7e2d4a
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2023-02-16 21:38:15 -0300

    CRUD con base de datos

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,23 +1,25 @@
-const fs = require('fs');
-const path = require('path');
+const db = require('../database/models');
 
 const dayjs = require('dayjs');
 dayjs.extend(require('dayjs/plugin/timezone'));
 dayjs.extend(require('dayjs/plugin/utc'));
 dayjs.tz.setDefault("America/Argentina/Buenos_Aires");
 
-let presupuestos = require('../data/presupuestos.json');
-const presupuestosDestino = path.join(__dirname, '../data/presupuestos.json');
-
 module.exports = {
     vistaCrear: (req, res) => {
         res.render('admin/crear');
     },
     vistaEditar: (req, res) => {
-        const {id} = req.params;
-        const presupuesto = presupuestos.find(presupuesto => presupuesto.id === parseInt(id));
 
-        res.render('admin/editar', { presupuesto })
+        db.Presupuesto.findByPk(+req.params.id)
+        .then((presupuesto) => {
+            res.render('admin/editar', { presupuesto });
+        })
+        .catch(error => {
+            console.log(error)
+            res.send(`No se pudo obtener el presupuesto de la base de datos \n${error}`);
+        })
+        
     },
     crearPresupuesto: (req, res) => {
         const { clave, nPresupuesto, precio0, precio1, precio2, precio3, precio4, precio5, precio6, precio7, precio8, precio9, precio10, precio11, descripcion } = req.body;
@@ -29,24 +31,27 @@ module.exports = {
             if (precio != "") {
                 totalPrecio += parseInt(precio)
             }
+        });
+
+        db.Presupuesto.create({
+            ...nuevoPresupuesto,
+            nPresupuesto: nPresupuesto == "" ? 1 : nPresupuesto,
+            clave: clave == "" ? 0 : clave,
+            fechaPresupuesto: dayjs.tz().format('DD/MM/YYYY'),
+            descripcion: descripcion == "" ? 0 : descripcion,
+            precioTrabajoTotal: totalPrecio
+        })
+        .then(presupuesto => {
+            res.redirect(`/presupuesto/${presupuesto.id}`);
+        })
+        .catch(error => {
+            res.send(`No se pudo crear el presupuesto ERROR => \n${error}`);
         })
 
-        nuevoPresupuesto.id = presupuestos.length + 1;
-        nuevoPresupuesto.nPresupuesto = nPresupuesto == "" ? 1 : nPresupuesto;
-        nuevoPresupuesto.clave = clave == "" ? 0 : clave;
-        nuevoPresupuesto.fechaPresupuesto = dayjs.tz().format('DD/MM/YYYY');
-        nuevoPresupuesto.descripcion = descripcion == "" ? 0 : descripcion;
-        nuevoPresupuesto.precioTrabajoTotal = totalPrecio;
-        
-        presupuestos.push(nuevoPresupuesto);
-        fs.writeFileSync(presupuestosDestino, JSON.stringify(presupuestos, null, 2));
-
-        res.redirect(`/presupuesto/${nuevoPresupuesto.id}`);
     },
     editarPresupuesto: (req, res) => {
 
-        let presupuestoEditado = presupuestos.find(presupuesto => presupuesto.id === parseInt(req.params.id));
-        const { clave, nombreCliente, direccionCliente, telefonoCliente, garantia, nPresupuesto, trabajo0, precio0, trabajo1, precio1, trabajo2, precio2, trabajo3, precio3, trabajo4, precio4, trabajo5, precio5, trabajo6, precio6, trabajo7, precio7, trabajo8, precio8, trabajo9, precio9, trabajo10, precio10, trabajo11, precio11, descripcion, descripcion0, descripcion1, descripcion2, descripcion3, descripcion4, descripcion5, descripcion6, descripcion7, descripcion8, descripcion9, descripcion10, descripcion11, actualizarFecha } = req.body;
+        const { clave, nombreCliente, direccionCliente, telefonoCliente, garantia, nPresupuesto, trabajo0, precio0, trabajo1, precio1, trabajo2, precio2, trabajo3, precio3, trabajo4, precio4, trabajo5, precio5, trabajo6, precio6, trabajo7, precio7, trabajo8, precio8, trabajo9, precio9, trabajo10, precio10, trabajo11, precio11, descripcion, descripcion0, descripcion1, descripcion2, descripcion3, descripcion4, descripcion5, descripcion6, descripcion7, descripcion8, descripcion9, descripcion10, descripcion11, actualizarFecha, fechaPresupuesto } = req.body;
 
         let totalPrecio = 0;
         let precios = [precio0, precio1, precio2, precio3, precio4, precio5, precio6, precio7, precio8, precio9, precio10, precio11];
@@ -54,65 +59,76 @@ module.exports = {
             if (precio != '') {
                 totalPrecio += parseInt(precio)
             }
+        });
+
+        db.Presupuesto.update({
+            clave: clave == "" ? 0 : clave,
+            nombreCliente: nombreCliente,
+            direccionCliente: direccionCliente,
+            telefonoCliente: telefonoCliente,
+            garantia: garantia,
+            nPresupuesto: nPresupuesto == "" ? 1 : nPresupuesto,
+            fechaPresupuesto: actualizarFecha == "on" ? dayjs.tz().format('DD/MM/YYYY') : fechaPresupuesto,
+            descripcion: descripcion == "" ? 0 : descripcion,
+            precioTrabajoTotal: totalPrecio,
+            trabajo0: trabajo0,
+            precio0: precio0,
+            descripcion0: descripcion0,
+            trabajo1: trabajo1,
+            precio1: precio1,
+            descripcion1: descripcion1,
+            trabajo2: trabajo2,
+            precio2: precio2,
+            descripcion2: descripcion2,
+            trabajo3: trabajo3,
+            precio3: precio3,
+            descripcion3: descripcion3,
+            trabajo4: trabajo4,
+            precio4: precio4,
+            descripcion4: descripcion4,
+            trabajo5: trabajo5,
+            precio5: precio5,
+            descripcion5: descripcion5,
+            trabajo6: trabajo6,
+            precio6: precio6,
+            descripcion6: descripcion6,
+            trabajo7: trabajo7,
+            precio7: precio7,
+            descripcion7: descripcion7,
+            trabajo8: trabajo8,
+            precio8: precio8,
+            descripcion8: descripcion8,
+            trabajo9: trabajo9,
+            precio9: precio9,
+            descripcion9: descripcion9,
+            trabajo10: trabajo10,
+            precio10: precio10,
+            descripcion10: descripcion10,
+            trabajo11: trabajo11,
+            precio11: precio11,
+            descripcion11: descripcion11
+
+        }, {
+            where: {id: req.params.id}
         })
-
-        actualizarFecha == "on" ? presupuestoEditado.fechaPresupuesto = dayjs.tz().format('DD/MM/YYYY') : null;
-
-        presupuestoEditado.palabraClave = clave == "" ? 0 : clave;
-        presupuestoEditado.nombreCliente = nombreCliente;
-        presupuestoEditado.direccionCliente = direccionCliente;
-        presupuestoEditado.telefonoCliente = telefonoCliente;
-        presupuestoEditado.garantia = garantia;
-        presupuestoEditado.nPresupuesto = nPresupuesto == "" ? 1 : nPresupuesto;
-        presupuestoEditado.descripcion = descripcion == "" ? 0 : descripcion;
-        presupuestoEditado.precioTrabajoTotal = totalPrecio;
-        presupuestoEditado.trabajo0 = trabajo0;
-        presupuestoEditado.precio0 = precio0;
-        presupuestoEditado.descripcion0 = descripcion0;
-        presupuestoEditado.trabajo1 = trabajo1;
-        presupuestoEditado.precio1 = precio1;
-        presupuestoEditado.descripcion1 = descripcion1;
-        presupuestoEditado.trabajo2 = trabajo2;
-        presupuestoEditado.precio2 = precio2;
-        presupuestoEditado.descripcion2 = descripcion2;
-        presupuestoEditado.trabajo3 = trabajo3;
-        presupuestoEditado.precio3 = precio3;
-        presupuestoEditado.descripcion3 = descripcion3;
-        presupuestoEditado.trabajo4 = trabajo4;
-        presupuestoEditado.precio4 = precio4;
-        presupuestoEditado.descripcion4 = descripcion4;
-        presupuestoEditado.trabajo5 = trabajo5;
-        presupuestoEditado.precio5 = precio5;
-        presupuestoEditado.descripcion5 = descripcion5;
-        presupuestoEditado.trabajo6 = trabajo6;
-        presupuestoEditado.precio6 = precio6;
-        presupuestoEditado.descripcion6 = descripcion6;
-        presupuestoEditado.trabajo7 = trabajo7;
-        presupuestoEditado.precio7 = precio7;
-        presupuestoEditado.descripcion7 = descripcion7;
-        presupuestoEditado.trabajo8 = trabajo8;
-        presupuestoEditado.precio8 = precio8;
-        presupuestoEditado.descripcion8 = descripcion8;
-        presupuestoEditado.trabajo9 = trabajo9;
-        presupuestoEditado.precio9 = precio9;
-        presupuestoEditado.descripcion9 = descripcion9;
-        presupuestoEditado.trabajo10 = trabajo10;
-        presupuestoEditado.precio10 = precio10;
-        presupuestoEditado.descripcion10 = descripcion10;
-        presupuestoEditado.trabajo11 = trabajo11;
-        presupuestoEditado.precio11 = precio11;
-        presupuestoEditado.descripcion11 = descripcion11;
-
-        fs.writeFileSync(presupuestosDestino, JSON.stringify(presupuestos, null, 2));
-
-        res.redirect(`/presupuesto/${req.params.id}`);
+        .then(() => {
+            res.redirect(`/presupuesto/${req.params.id}`);
+        })
+        .catch(error => {
+            res.send(`No se pudo editar el presupuesto \n${error}`);
+        })
 
     },
     eliminarPresupuesto: (req, res) => {
-        presupuestos = presupuestos.filter(e => e.id !== parseInt(req.params.id));
 
-        fs.writeFileSync(presupuestosDestino, JSON.stringify(presupuestos, null, 2));
-
-		res.redirect("/");
+        db.Presupuesto.destroy({
+            where: {id: req.params.id}
+        })
+        .then( () => {
+            res.redirect("/");
+        })
+        .catch(error => {
+            res.send(`No se pudo eliminar el presupuesto \n${error}`);
+        })
     }
 }
--- src/controllers/detailController.js
+++ src/controllers/detailController.js
@@ -1,11 +1,16 @@
-let presupuestos =require("../data/presupuestos.json");
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const db = require('../database/models');
 
 module.exports = {
     detail: (req, res) => {
-        const {id} = req.params;
-        const presupuesto = presupuestos.find(presupuesto => presupuesto.id === parseInt(id));
 
-        res.render('detail/detail', { presupuesto, toThousand });
+        db.Presupuesto.findByPk(+req.params.id)
+        .then(presupuesto => {
+            res.render('detail/detail', { presupuesto, toThousand })
+        })
+        .catch(error => {
+            console.log(error)
+            res.send("No se pudo obtener el presupuesto de la base de datos")
+        })
     }
 }
